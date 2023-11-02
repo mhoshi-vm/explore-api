5a7211d7725590edad5d9272d4536fa45bb2e13e
machih <machih@vmware.com>
2023-11-02 22:43:56 +0900

    Finish v1 with SSE

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
 
-
 INSERT INTO summary (session_id, title, content) VALUES ('A1', 'Session1', 'AAAA');
 INSERT INTO summary (session_id, title, content) VALUES ('B1', 'Session2', 'BBBB');
 INSERT INTO summary (session_id, title, content) VALUES ('C1', 'Session3', 'CCCC');
